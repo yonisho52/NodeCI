33d99d3c265e14c55e6469a60a3fce753d60e7c0
Yonatan Shomer <yonisho52@gmail.com>
2023-04-18 23:52:36 +0300

    Initial commit

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+      '',
+    googleClientSecret: '',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '',
+    redisUrl: 'redis://127.0.0.1:6379'
+  };
---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '',
+  googleClientSecret: '',
+  mongoURI: '',
+  cookieKey: '',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
